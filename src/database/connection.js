5d58dc346a2c817b10ca7234f5580182f02e4517
faruqismael <farusmith5@gmail.com>
2023-02-09 11:24:28 +0300

    db connection for both server & local

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,3 +6,18 @@ const sequelize = new Sequelize("istyle", "root", "root", {
 
 module.exports = sequelize;
 global.sequelize = sequelize;
+
+// server;
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(
+//  'istyle',
+//  'istyle_admin',
+//  'Istyleaddis=1',
+//   {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//   }
+// );
+
+// module.exports = sequelize
+// global.sequelize = sequelize
